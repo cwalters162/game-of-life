fde819e08d60465dc878ad3a9c90e24ee9446502
Cherokee Walters <cherokeewalters162@gmail.com>
2023-12-21 23:50:57 -0500

    Replaced Vec of cells with BTreeMap with resulting refactor requirements to allow boundless canvas

--- src/cell.rs
+++ src/cell.rs
@@ -1,12 +1,11 @@
 use macroquad::color::{RED, WHITE};
-use macroquad::prelude::draw_rectangle;
+use macroquad::prelude::{BLACK, Color, draw_rectangle};
 use macroquad::shapes::draw_rectangle_lines;
 
+#[derive(Clone)]
 pub struct Cell {
     grid_x: i32,
     grid_y: i32,
-    pub alive: bool,
-    pub next_alive: bool,
 }
 
 impl Cell {
@@ -14,33 +13,24 @@ impl Cell {
         Cell {
             grid_x,
             grid_y,
-            alive: macroquad::rand::gen_range(0, 100) > 50,
-            next_alive: false,
         }
     }
     pub fn draw(&mut self) {
         let scale = 1f32;
-        if self.alive {
-            draw_rectangle(
-                self.grid_x as f32 * scale,
-                self.grid_y as f32 * scale,
-                scale,
-                scale,
-                WHITE,
-            );
-            draw_rectangle_lines(self.grid_x as f32 * scale,
-                                 self.grid_y as f32 * scale,
-                                 scale,
-                                 scale,
-                                 scale / 10.,
-                                 RED)
-        }
-    }
-
-    pub fn toggle_alive(&mut self) -> () {
-        self.alive = !self.alive
-    }
-    pub fn toggle_next_alive(&mut self) -> () {
-        self.next_alive = !self.next_alive
+        draw_rectangle(
+            self.grid_x as f32 * scale,
+            self.grid_y as f32 * scale,
+            scale,
+            scale,
+            WHITE,
+        );
+        draw_rectangle_lines(
+            self.grid_x as f32 * scale,
+            self.grid_y as f32 * scale,
+            scale,
+            scale,
+            scale / 10.,
+            BLACK
+        );
     }
 }
--- src/game_world.rs
+++ src/game_world.rs
@@ -1,14 +1,14 @@
-use macroquad::camera::Camera;
+use std::collections::{BTreeMap, HashMap, HashSet};
 use macroquad::input::{is_mouse_button_pressed, mouse_position, MouseButton};
-use macroquad::prelude::{Camera2D, screen_height, screen_width, Vec2};
-use macroquad::prelude::scene::camera_pos;
-use macroquad::rand::{gen_range};
+use macroquad::prelude::{Camera2D, get_time, screen_height, screen_width, Vec2, vec2};
+use macroquad::rand::{gen_range, rand, srand};
 use crate::cell::Cell;
 
+#[derive(Clone)]
 pub struct GameWorld {
     pub rows: i32,
     pub cols: i32,
-    pub cells: Vec<Vec<Cell>>
+    pub cells: BTreeMap<Pos2d, Cell>
 }
 
 impl GameWorld {
@@ -22,69 +22,143 @@ impl GameWorld {
         }
     }
 
-    pub fn create_grid(rows: i32, columns: i32) -> Vec<Vec<Cell>> {
-        let mut cells = Vec::<Vec<Cell>>::new();
+    pub fn create_grid(rows: i32, columns: i32) -> BTreeMap<Pos2d, Cell> {
+        let mut grid: BTreeMap<Pos2d, Cell> = BTreeMap::new();
+        let x_left = columns / 2 * -1;
+        let x_right = columns / 2;
+        let y_top = rows / 2 * -1;
+        let y_bottom = rows / 2;
 
-        for r in 0..rows{
-            cells.push(Vec::<Cell>::new());
-            for c in 0..columns {
-                cells[r as usize].push(Cell::new(r, c))
-            }
-        }
-
-        cells
-    }
-
-    pub fn is_alive(&self, x: i32, y: i32) -> i32 {
-        if x < 0 || x >= self.rows || y < 0 || y >= self.cols {
-            return 0;
-        }
-
-        return if self.cells[x as usize][y as usize].alive {
-            1
-        } else {
-            0
-        };
-    }
-
-    pub fn check_surrounding(&mut self) {
-        for x in 0..self.cols {
-            for y in 0..self.rows {
-                if x < 0 || x >= self.rows || y < 0 || y >= self.cols {
-                    continue;
-                }
-
-                let num_alive = self.is_alive(x - 1, y - 1)
-                    + self.is_alive(x, y - 1)
-                    + self.is_alive(x + 1, y - 1)
-                    + self.is_alive(x - 1, y)
-                    + self.is_alive(x + 1, y)
-                    + self.is_alive(x - 1, y + 1)
-                    + self.is_alive(x, y + 1)
-                    + self.is_alive(x + 1, y + 1);
-
-                if num_alive == 2 {
-                    self.cells[x as usize][y as usize].next_alive =
-                        self.cells[x as usize][y as usize].alive;
-                } else if num_alive == 3 {
-                    self.cells[x as usize][y as usize].next_alive = true;
-                } else {
-                    self.cells[x as usize][y as usize].next_alive = false;
+        for y in y_top..y_bottom{
+            for x in x_left..x_right {
+                if gen_range(1, 100) > 50 {
+                    grid.insert(Pos2d { x, y }, Cell::new(x, y));
                 }
             }
         }
-        for r in 0..self.cells.len() {
-            for c in 0..self.cells[r].len() {
-                self.cells[r][c].alive = self.cells[r][c].next_alive;
+
+        grid
+    }
+
+    pub fn is_alive(&self, x: i32, y: i32) -> bool {
+        self.cells.contains_key(&Pos2d { x, y })
+    }
+
+    //TODO: """Once Binary Tree is introduced be sure to change this so that
+    // if there is a living cell and any of the neighboring "missing cells"
+    // has 2 or more living neighbors to then be inserted one all the cells have been checked.
+    // which means there will likely be a Vec of positions to insert new living cells at the end of the checks.
+    pub fn check_cells(&mut self) {
+        let mut pos_to_remove = Vec::<Pos2d>::new();
+        let mut new_cells = HashMap::<Pos2d, Cell>::new();
+        // Check the current cells neighbors,
+        // if it's alive move on
+        // if it's dead, check it's neighbors, add to the pos list of checked cells
+        // if it's in that list check if it needs to be a new cell or not
+        // set current cell dead or alive.
+        for (pos, _) in self.cells.clone() {
+            //-1, -1 | 0,-1 | 1, -1
+            //-1,  0 | 0, 0 | 1,  0
+            //-1,  1 | 0, 1 | 1,  1
+
+            let mut num_alive = 0;
+            if self.is_alive(pos.x - 1, pos.y - 1) {
+                    num_alive += 1;
+            } else {
+                match self.number_of_living_neighbors(&Pos2d {x: pos.x - 1, y: pos.y - 1}) {
+                    3 => {
+                        new_cells.insert(Pos2d {x: pos.x - 1, y: pos.y - 1}, Cell::new(pos.x - 1, pos.y - 1));
+                    }
+                    _ => {}
+                }
             }
+            if self.is_alive(pos.x, pos.y - 1) {
+                num_alive += 1;
+            } else {
+                match self.number_of_living_neighbors(&Pos2d {x: pos.x, y: pos.y - 1}) {
+                    3 => {
+                        new_cells.insert(Pos2d {x: pos.x, y: pos.y - 1}, Cell::new(pos.x, pos.y - 1));
+                    }
+                    _ => {}
+                }
+            }
+            if self.is_alive(pos.x + 1, pos.y - 1) {
+                num_alive += 1;
+            } else {
+                match self.number_of_living_neighbors(&Pos2d {x: pos.x + 1, y: pos.y - 1}) {
+                    3 => {
+                        new_cells.insert(Pos2d {x: pos.x + 1, y: pos.y - 1}, Cell::new(pos.x + 1, pos.y - 1));
+                    }
+                    _ => {}
+                }
+            }
+            if self.is_alive(pos.x - 1, pos.y) {
+                num_alive += 1;
+            } else {
+                match self.number_of_living_neighbors(&Pos2d {x: pos.x - 1, y: pos.y }) {
+                    3 => {
+                        new_cells.insert(Pos2d {x: pos.x - 1, y: pos.y }, Cell::new(pos.x - 1, pos.y));
+                    }
+                    _ => {}
+                }
+            }
+            if self.is_alive(pos.x + 1, pos.y) {
+                num_alive += 1;
+            } else {
+                match self.number_of_living_neighbors(&Pos2d {x: pos.x + 1, y: pos.y }) {
+                    3 => {
+                        new_cells.insert(Pos2d {x: pos.x + 1, y: pos.y }, Cell::new(pos.x + 1, pos.y));
+                    }
+                    _ => {}
+                }
+            }
+            if self.is_alive(pos.x - 1, pos.y + 1) {
+                num_alive += 1;
+            } else {
+                match self.number_of_living_neighbors(&Pos2d {x: pos.x - 1, y: pos.y + 1}) {
+                    3 => {
+                        new_cells.insert(Pos2d {x: pos.x - 1, y: pos.y + 1}, Cell::new(pos.x - 1, pos.y + 1));
+                    }
+                    _ => {}
+                }
+            }
+            if self.is_alive(pos.x, pos.y + 1) {
+                num_alive += 1;
+            } else {
+                match self.number_of_living_neighbors(&Pos2d {x: pos.x, y: pos.y + 1 }) {
+                    3 => {
+                        new_cells.insert(Pos2d {x: pos.x, y: pos.y + 1 }, Cell::new(pos.x, pos.y + 1));
+                    }
+                    _ => {}
+                }
+            }
+            if self.is_alive(pos.x + 1, pos.y + 1) {
+                num_alive += 1;
+            } else {
+                match self.number_of_living_neighbors(&Pos2d {x: pos.x + 1, y: pos.y + 1}) {
+                    3 => {
+                        new_cells.insert(Pos2d {x: pos.x + 1, y: pos.y + 1}, Cell::new(pos.x + 1, pos.y + 1));
+                    }
+                    _ => {}
+                }
+            }
+            if num_alive == 2 || num_alive == 3 {
+
+            } else {
+                pos_to_remove.push(pos);
+            }
+        }
+        for pos in pos_to_remove {
+            self.cells.remove(&pos);
+        }
+        for (pos, cell) in new_cells {
+            self.cells.insert(pos, cell);
         }
     }
 
     pub fn draw_cells(&mut self) {
-        for row in self.cells.iter_mut() {
-            for mut cell in row {
-                cell.draw();
-            }
+        for (_, cell) in self.cells.iter_mut() {
+            cell.draw();
         }
     }
 
@@ -94,53 +168,72 @@ impl GameWorld {
             let (mouse_x, mouse_y) = mouse_position();
             let screen_to_world = cam.screen_to_world(Vec2::from(mouse_position()));
 
-            let x = screen_to_world.x as i32;
-            let y = screen_to_world.y as i32;
-            dbg!("Mouse X {} Mouse Y {}", mouse_x, mouse_y);
-            dbg!("Adjusted X {} Adjusted Y {}", mouse_x, mouse_y);
-            dbg!("x: {}, y: {}", x, y);
+            let mut x = screen_to_world.x.floor() as i32;
+            let mut y = screen_to_world.y.floor() as i32;
 
-            if x >= self.cols && y >= self.rows {
-                let not_out_of_bounds_x = x - self.cols;
-                let not_out_of_bounds_y = y - self.rows;
-                let mut cell = &mut self.cells[(x - not_out_of_bounds_x - 1) as usize]
-                    [(y - not_out_of_bounds_y - 1) as usize];
-                cell.toggle_alive();
-                cell.toggle_next_alive();
-            } else if y >= self.rows {
-                let not_out_of_bounds = y - self.rows;
-                let mut cell = &mut self.cells[x as usize][(y - not_out_of_bounds - 1) as usize];
-                cell.toggle_alive();
-                cell.toggle_next_alive();
-            } else if x >= self.cols {
-                let not_out_of_bounds = x - self.cols;
-                let mut cell = &mut self.cells[(x - not_out_of_bounds - 1) as usize][y as usize];
-                cell.toggle_alive();
-                cell.toggle_next_alive();
-            } else {
-                let mut cell = &mut self.cells[x as usize][y as usize];
-                cell.toggle_alive();
-                cell.toggle_next_alive();
+            if cfg!(debug_assertions) {
+                println!("Mouse X {} Mouse Y {}", mouse_x, mouse_y);
+                println!("Screen_to_world X {} Mouse Y {}", screen_to_world.x, screen_to_world.y);
+                println!("x: {}, y: {}", x, y);
+            }
+
+            let pos = Pos2d {x, y};
+
+            match self.cells.contains_key(&pos) {
+                false => {
+                    self.cells.insert(pos, Cell::new(x, y));
+                }
+                true => {
+                    self.cells.remove(&pos);
+                }
             }
         }
     }
 
     pub fn clear(&mut self) {
-        for row in self.cells.iter_mut() {
-            for mut cell in row {
-                cell.alive = false;
-            }
-        }
+        self.cells.clear();
     }
     pub fn randomize(&mut self) {
-        for row in self.cells.iter_mut() {
-            for mut cell in row {
-                cell.alive = gen_range(0, 10) >= 5;
-            }
-        }
+        self.cells = GameWorld::create_grid(self.rows, self.cols)
     }
 
     pub fn update(&mut self) {
-        self.check_surrounding();
+        self.check_cells();
     }
+
+    fn number_of_living_neighbors(&self, pos: &Pos2d) -> i32 {
+        let mut num_alive = 0;
+        if self.is_alive(pos.x - 1, pos.y - 1) {
+            num_alive += 1;
+        }
+        if self.is_alive(pos.x, pos.y - 1) {
+            num_alive += 1;
+        }
+        if self.is_alive(pos.x + 1, pos.y - 1) {
+            num_alive += 1;
+        }
+        if self.is_alive(pos.x - 1, pos.y) {
+            num_alive += 1;
+        }
+        if self.is_alive(pos.x + 1, pos.y) {
+            num_alive += 1;
+        }
+        if self.is_alive(pos.x - 1, pos.y + 1) {
+            num_alive += 1;
+        }
+        if self.is_alive(pos.x, pos.y + 1) {
+            num_alive += 1;
+        }
+        if self.is_alive(pos.x + 1, pos.y + 1) {
+            num_alive += 1;
+        }
+        num_alive
+    }
+}
+
+#[derive(PartialOrd, PartialEq, Ord, Eq, Clone, Debug)]
+#[derive(Hash)]
+pub struct Pos2d {
+    x: i32,
+    y: i32,
 }
